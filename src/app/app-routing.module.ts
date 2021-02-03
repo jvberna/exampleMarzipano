5aab16160d50d31ebc66760e61850036766c9f9d
Jose Vicente Berna <jvberna@ua.es>
2021-02-03 08:57:44 +0100

    Ejemplo uso Marzipano

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VisorComponent } from './visor/visor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'visor', component: VisorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VisorComponent } from './visor/visor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisorComponent
   ],
   imports: [
     BrowserModule,
